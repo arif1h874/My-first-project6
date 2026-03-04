e91e0ca0c7fd65fe056f1f1d3a95e6b799ebff79
arif1h874 <arif200a8@gmail.com>
2026-03-04 17:21:23 +0600

    add a OpenAI api

--- client.py
+++ client.py
@@ -2,7 +2,9 @@ from openai import OpenAI
  
 # pip install openai 
 # if you saved the key under a different environment variable name, you can do something like:
-
+client = OpenAI(
+  api_key="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",
+)
 
 completion = client.chat.completions.create(
   model="gpt-3.5-turbo",
