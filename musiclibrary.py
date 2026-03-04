b41a0aee52f512551220ee64e902ada04deec3a9
arif1h874 <arif200a8@gmail.com>
2026-03-04 17:16:22 +0600

    This is musiclibrary

---
+++ musiclibrary.py
@@ -0,0 +1,16 @@
+music={
+    "sath":"https://www.youtube.com/watch?v=8yK-56GCafA&list=RD8yK-56GCafA&start_radio=1",
+    "Ehsass":"https://www.youtube.com/results?search_query=Essah",
+    "sad song":"https://www.youtube.com/results?search_query=sad+song",
+    "attitude":"https://www.youtube.com/results?search_query=attitude+song",
+    "saiyaara":"https://www.youtube.com/watch?v=BSJa1UytM8w&list=RDBSJa1UytM8w&start_radio=1",
+    "romantic":"https://www.youtube.com/results?search_query=hot+song",
+    "waz":"https://www.youtube.com/results?search_query=waz",
+    "funny":"https://www.youtube.com/results?search_query=funny+content" ,
+    "tiktok":"https://www.tiktok.com/en/",
+    "python":"https://www.w3schools.com/"
+}
+   
+  
+
+  
